70d365cab09e08315caa544d907de71cb7122062
Abdul Rehman <abdulrehman@Abduls-MacBook-Pro.local>
2017-11-02 08:36:30 +0500

    rtl support added

--- app/src/main/java/org/khidmatalnaas/shetanidhang/MainActivity.kt
+++ app/src/main/java/org/khidmatalnaas/shetanidhang/MainActivity.kt
@@ -2,11 +2,13 @@ package org.khidmatalnaas.shetanidhang
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
         setContentView(R.layout.activity_main)
     }
 }
